150542540e2be034fe71bdb9f6637d5a8f00efc0
Mazen <mazenadel19@gmail.com>
2024-10-05 21:38:17 +0300

    fix: wrong api key

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -4,7 +4,7 @@ import { getDatabase } from 'firebase/database'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_APP_ID,
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
