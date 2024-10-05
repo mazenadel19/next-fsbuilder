55f849e8eefca305722601a127184e87d09bad61
Mazen <mazenadel19@gmail.com>
2024-10-05 21:47:51 +0300

    feat: firebase with client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+'use client'
 import { Button } from '@mui/material'
 import { fetchData, writeData } from './utils/actions'
 
-export default async function Home() {
-    writeData()
-    const response = await fetchData()
-    console.log(response)
+export default function Home() {
+    const handleClick = async () => {
+        await writeData()
+        const response = await fetchData()
+        console.log(response)
+    }
+
     return (
         <div>
-            <Button variant="contained">Hello world</Button>
+            <Button variant="contained" onClick={handleClick}>
+                Hello world
+            </Button>
         </div>
     )
 }
