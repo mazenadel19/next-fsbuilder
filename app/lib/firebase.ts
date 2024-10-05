74ed4ad061b7221d5f36bcd48698e0f3592eb9a5
Mazen <mazenadel19@gmail.com>
2024-10-05 21:08:46 +0300

    chore: add Firebase configurations

---
+++ app/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_APP_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getDatabase(app)
+
+export { db }
